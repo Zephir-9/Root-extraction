cfa124e99f2ace420e52d80c5902a738dcfca138
Zephyr <37902452+Zephir-9@users.noreply.github.com>
2019-08-18 23:42:39 +0300

    Add files via upload

---
+++ root.cpp
@@ -0,0 +1,28 @@
+﻿//решение системы линейных уравнений с двумя неизвестными методом Крамера
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{setlocale(LC_ALL, "RUS");
+double numeric = NULL;
+
+std::cout << "Введите число: ";
+std::cin >> numeric;
+
+system("cls");
+
+double root = numeric / 2;
+double eps = 0.01;
+int iter = 0;
+
+while (0.5 * (root - numeric / root) > eps)
+{
+	iter++;
+	root = 0.5 * (root + numeric / root);
+	
+}
+    std::cout << "Корень: " << root << '\n';
+	system("PAUSE");
+	return 0;
+}
